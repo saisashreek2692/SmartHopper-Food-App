3fc92572632967e30d808d04d3ab974b939c68e6
Sai Sashreek <saisashreekd@gmail.com>
2024-04-22 09:09:46 +0530

    ShadCN Components File

---
+++ app/_components/Header.jsx
@@ -0,0 +1,38 @@
+"use client";
+import { Search } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs";
+
+function Header() {
+  const { user, isSignedIn } = useUser();
+
+  return (
+    <div className="flex justify-between items-center md:px-20 shadow-sm">
+      <Image src="/logo.png" alt="logo" width={200} height={200} />
+      <div className="hidden md:flex border p-2 rounded-lg bg-gray-200 w-96">
+        <input
+          type="text"
+          className="bg-transparent w-full outline-none border-none"
+          placeholder="Enter to Search Products"
+        />
+        <Search />
+      </div>
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <div className="flex gap-5">
+          <SignInButton mode="modal">
+            <Button variant="outline">Sign In</Button>
+          </SignInButton>
+          <SignUpButton mode="modal">
+            <Button>Sign Up</Button>
+          </SignUpButton>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Header;
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,10 @@
-import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Quicksand } from "next/font/google";
 import "./globals.css";
+import Provider from "./Provider";
 
-const inter = Inter({ subsets: ["latin"] });
+
+const inter = Quicksand({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -10,8 +13,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Provider>
+            {children}
+          </Provider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
