b48175b1186003c0045d313033ecffe8909b3314
Sai Sashreek <saisashreekd@gmail.com>
2024-04-22 18:09:20 +0530

    NextJS Config File Updated with HyGraph

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['ap-south-1.graphassets.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'ap-south-1.graphassets.com',
+                port: '',
+                pathname: '/**'
+            }
+        ]
+    }
+};
 
 export default nextConfig;
