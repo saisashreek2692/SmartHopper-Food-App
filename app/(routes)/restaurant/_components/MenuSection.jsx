bebc06ed79379bc7753c9cf17ae6c33f1eed7ed4
Sai Sashreek <saisashreekd@gmail.com>
2024-04-23 17:25:50 +0530

    Cart Item Count Added

--- app/(routes)/restaurant/_components/MenuSection.jsx
+++ app/(routes)/restaurant/_components/MenuSection.jsx
@@ -1,12 +1,18 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-"use client"
+"use client";
+import { CartUpdateContext } from "@/app/_context/CartUpdateContext";
+import GlobalApi from "@/app/_utils/GlobalApi";
 import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/nextjs";
 import { SquarePlus } from "lucide-react";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { toast } from "sonner";
 
 function MenuSection({ restaurant }) {
   const [menuItemList, setMenuItemList] = useState([]);
+  const { user } = useUser();
+  const {updateCart, setUpdateCart} = useContext(CartUpdateContext);
 
   useEffect(() => {
     restaurant?.menu && FilterMenu(restaurant?.menu[0]?.category);
@@ -17,6 +23,25 @@ function MenuSection({ restaurant }) {
       (item) => item.category === category
     );
     setMenuItemList(result[0]);
+  };
+
+  const addToCartHandler = (item) => {
+    toast('Adding to Cart');
+    const data = {
+      email: user?.primaryEmailAddress?.emailAddress,
+      name: item?.name,
+      description: item?.description,
+      productImage: item?.productImage?.url,
+      price: item?.price,
+      restaurantSlug: restaurant.slug
+    };
+    GlobalApi.AddToCart(data).then(resp => {
+        console.log(resp);
+        setUpdateCart(!updateCart);
+        toast('Added to Cart');
+    }, (error) => {
+        toast('Adding to Cart Failed');
+    })
   };
 
   return (
@@ -35,10 +60,13 @@ function MenuSection({ restaurant }) {
           ))}
         </div>
         <div className="md:col-span-3 col-span-4">
-          <h2 className="font-extrabold text-lg">{menuItemList.category}</h2>
+          <h2 className="font-extrabold text-lg">{menuItemList?.category}</h2>
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-5">
             {menuItemList?.menuItem?.map((item, index) => (
-              <div key={index} className="p-2 flex gap-3 border rounded-xl hover:border-primary cursor-pointer">
+              <div
+                key={index}
+                className="p-2 flex gap-3 border rounded-xl hover:border-primary cursor-pointer"
+              >
                 <Image
                   src={item?.productImage?.url}
                   alt={item.name}
@@ -47,10 +75,15 @@ function MenuSection({ restaurant }) {
                   className="w-[120px] h-[120px] object-cover rounded-xl"
                 />
                 <div className="flex flex-col gap-1">
-                    <h2 className="font-bold">{item.name}</h2>
-                    <h2>&#8377; {item.price}</h2>
-                    <h2 className="text-sm text-gray-400 line-clamp-2">{item.description}</h2>
-                    <SquarePlus />
+                  <h2 className="font-bold">{item.name}</h2>
+                  <h2>&#8377; {item.price}</h2>
+                  <h2 className="text-sm text-gray-400 line-clamp-2">
+                    {item.description}
+                  </h2>
+                  <SquarePlus
+                    className="cursor-pointer"
+                    onClick={() => addToCartHandler(item)}
+                  />
                 </div>
               </div>
             ))}
