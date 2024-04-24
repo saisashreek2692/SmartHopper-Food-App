957cd62db88cc8c35978a0867a707039502ae4d1
Sai Sashreek <saisashreekd@gmail.com>
2024-04-24 16:47:16 +0530

    Ratings added to every restaurant

--- app/(routes)/restaurant/_components/Intro.jsx
+++ app/(routes)/restaurant/_components/Intro.jsx
@@ -1,8 +1,30 @@
 import { MapPin } from "lucide-react";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Intro({ restaurant }) {
+
+  const [totalReview, setTotalReview] = useState();
+  const [avgRating, setAvgRating] = useState();
+
+  useEffect(() => {
+    restaurant && calculateRating();
+  }, [restaurant]);
+
+  const calculateRating = () => {
+    let total = 0;
+    let count = 0;
+    restaurant?.reviews?.forEach(item => {
+      total = total + item.star;
+      count++;
+    });
+
+    setTotalReview(count);
+    const result = total / count;
+    setAvgRating(result ? result.toFixed(1) : 0);
+
+  };
+
   return (
     <div>
       {restaurant?.banner?.url ? (
@@ -21,7 +43,7 @@ function Intro({ restaurant }) {
       <h2 className="text-3xl font-bold mt-2">{restaurant.name}</h2>
       <div className="flex items-center gap-2 mt-2">
         <Image src={"/star.png"} alt="star" width={20} height={20} priority={true} />
-        <label className="text-sm text-gray-500">4.5 (56)</label>
+        <label className="text-sm text-gray-500">{avgRating} ({totalReview})</label>
       </div>
       <h2 className="flex gap-2 text-gray-500 items-center mt-2">
         <MapPin /> {restaurant.address}
